c54e754cbd6c697e9e3fbc3570329142cd9814f3
Skyler Garrison <42485938+skylergarrison@users.noreply.github.com>
2020-03-09 20:35:54 -0700

    adding first button

--- app/src/main/java/com/example/pokemontcg/MainActivity.kt
+++ app/src/main/java/com/example/pokemontcg/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.pokemontcg
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun changeText(view: View) {
+        val tv1 = findViewById(R.id.hellotext) as TextView
+        tv1.text = "Pokemon!"
     }
 }
